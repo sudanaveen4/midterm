dbc63ce04f83ec967aed34e34c34e0310171a925
suda <99373763+sudanaveen4@users.noreply.github.com>
2023-11-21 18:07:02 +0530

    aslhgjsd

--- src/main/java/com/vit/login/Login.java
+++ src/main/java/com/vit/login/Login.java
@@ -3,27 +3,9 @@ import java.util.Map;
 
 public class Login {
 
-    private static Map<String, String> users = new HashMap<>();
-
-    static {
-        users.put("ram", "3025");
-    }
-
-    public static boolean login(String username, String ID) {
-        String expectedID = users.get(username);
-        return expectedID != null && expectedID.equals(ID);
-    }
-
     public static void main(String[] args) {
-        String username = "suda";
-        String ID = "sudatheking";
-
-        boolean loggedIn = login(username, ID);
-
-        if (loggedIn) {
-            System.out.println("Login successful!");
-        } else {
-            System.out.println("Login failed.");
-        }
+    	
+        System.out.println("Login failed.");
+        
     }
 }
